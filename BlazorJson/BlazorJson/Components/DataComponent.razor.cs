82d679ab37ee047d9c5f80536419b0dec800d5b0
RicardoJosuePerezAltamirano <ricardoj.altamirano@outlook.com>
2023-11-25 22:18:38 -0600

    init

---
+++ BlazorJson/BlazorJson/Components/DataComponent.razor.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorJson.Components
+{
+    public partial class DataComponent
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        public List<Model> DataModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var data = await Client.
+                GetFromJsonAsync<List<Model>>
+                ("https://raw.githubusercontent.com/BryanOroxon/data/main/pets.json");
+            if (data != null)
+            {
+                DataModel = data;
+                
+            }
+        }
+    }
+}
---
+++ BlazorJson/BlazorJson/Model.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorJson
+{
+    public class Model
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Breed { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Size { get; set; }
+        public string Gender { get; set; }
+        public string Health { get; set; }
+        public string Meet { get; set; }
+        public string Social { get; set; }
+        public string Url { get; set; }
+    }
+}
